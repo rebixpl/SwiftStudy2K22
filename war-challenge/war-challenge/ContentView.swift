107de93e4d607fc34c8ea2d7707b01e6eadbd25b
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2021-10-08 22:51:22 +0200

    finished war game ui challenge

---
+++ war-challenge/war-challenge/ContentView.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  war-challenge
+//
+//  Created by rebix on 08/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            // Background
+            Image("background").resizable()
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            // Game UI
+            VStack(alignment: .center) {
+                Spacer()
+                Image("logo")
+                Spacer()
+                
+                // Cards
+                HStack(spacing: 60.0) {
+                    // Card 1
+                    Image("card3")
+                        .cornerRadius(6.0)
+                    
+                    // Card 2
+                    Image("card4")
+                        .cornerRadius(6.0)
+                }
+                Spacer()
+                
+                // Deal Button
+                Image("dealbutton")
+                Spacer()
+                
+                // Player and CPU scores
+                HStack(spacing:120.0){
+                    VStack(spacing:10.0){
+                        Text("Player")
+                            .fontWeight(.bold).padding(.bottom,10.0)
+                        Text("0").font(.title).fontWeight(.semibold)
+                    }
+                    VStack(spacing:10.0){
+                        Text("CPU")
+                            .fontWeight(.bold).padding(.bottom,10.0)
+                        Text("0").font(.title).fontWeight(.semibold)
+                    }
+                }.foregroundColor(.white)
+                Spacer()
+            }
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
