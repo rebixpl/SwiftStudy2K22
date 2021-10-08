0aa72f682d139f852e7fabbdfa8dbf7e2bea8169
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2021-10-08 07:44:57 +0200

    init

---
+++ uitest/uitest/uitestApp.swift
@@ -0,0 +1,17 @@
+//
+//  uitestApp.swift
+//  uitest
+//
+//  Created by rebix on 08/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct uitestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
