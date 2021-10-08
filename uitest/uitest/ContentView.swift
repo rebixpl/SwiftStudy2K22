ba7a96c1bd93a68b2a762ea29561d2ec6d11613d
Krystian Gułaś <33410494+rebixpl@users.noreply.github.com>
2021-10-08 10:27:21 +0200

    Image(), Spacer() and Stacks

--- uitest/uitest/ContentView.swift
+++ uitest/uitest/ContentView.swift
@@ -9,8 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            
+            Spacer()
+            
+            Image("logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            Spacer()
+            
+            HStack ( alignment:.center,spacing: 0.0){
+                Spacer()
+                Text("Play Game")
+                    .fontWeight(.bold)
+                    .padding(.all)
+                    .background(Color.green)
+                    .foregroundColor(Color.white)
+                    .cornerRadius(/*@START_MENU_TOKEN@*/13.0/*@END_MENU_TOKEN@*/)
+                    .shadow(radius: 3)
+                Spacer()
+                Text("Settings")
+                    .fontWeight(.bold)
+                    .padding(.all)
+                    .background(Color.red)
+                    .foregroundColor(Color.white)
+                    .cornerRadius(/*@START_MENU_TOKEN@*/13.0/*@END_MENU_TOKEN@*/)
+                    .shadow(radius: 3)
+                Spacer()
+            }
+            
+            Spacer()
+        }
+        
+        //        Text("Hello, World!")
+        //            .fontWeight(.bold)
+        //            .padding(.all)
+        //            .background(Color.green)
+        //            .foregroundColor(Color.white)
+        //            .cornerRadius(/*@START_MENU_TOKEN@*/13.0/*@END_MENU_TOKEN@*/)
+        //            .shadow(radius: 3)
+        //            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        //            .background(Color.blue)
+        //            .cornerRadius(13.0)
+        //        .shadow(radius:3.0)
+        //
+        
     }
 }
 
